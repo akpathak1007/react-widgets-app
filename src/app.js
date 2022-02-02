77d59186d8c96c6feb770cde7df1c68a97adef1a
akpathak1007 <anujkumarpathak1007@gmail.com>
2022-02-02 17:49:52 +0530

    pretty basic routing using functions

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import items from "./data/accordion";
 import dropdownOptions from "./data/dropdown";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const App = () => {
+  const accordion = () => {
+    if (window.location.pathname === "/") {
+      return <Accordion items={items} />;
+    }
+  };
+  const list = () => {
+    if (window.location.pathname === "/list") {
+      return <Search />;
+    }
+  };
+  const dropdown = () => {
+    if (window.location.pathname === "/dropdown") {
+      return <Dropdown />;
+    }
+  };
+  const translate = () => {
+    if (window.location.pathname === "/translate") {
+      return <Translate />;
+    }
+  };
   return (
     <div className="container mt-4">
-      <Translate />
+      {accordion()}
+      {dropdown()}
+      {translate()}
+      {list()}
     </div>
   );
 };
